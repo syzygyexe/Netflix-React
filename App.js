d7b8368a7eef4c11fa7fdf5997bae49d08c6cb74
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-05 20:49:41 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import JumboData from "./fixtures/jumbo";
+import Jumbotron from "./components/jumbotron/index";
+
+function App() {
+  return (
+    <Jumbotron.Container>
+      {JumboData.map((item) => (
+        // We are taking item.id and item.direction from jumbo.json
+        <Jumbotron key={item.id} direction={item.direction}>
+          {/* Left hand-side */}
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          {/* Right hand-side */}
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
+}
+
+export default App;
