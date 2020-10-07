42eefa833319cda3c838de0613686d067990a7ae
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-07 21:02:15 +0900

    Add files via upload

---
+++ containers/jumbotron.js
@@ -0,0 +1,24 @@
+import React from "react";
+import JumboData from "../fixtures/jumbo";
+import { Jumbotron } from "../components";
+
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      {JumboData.map((item) => (
+        // We are taking item.id and item.direction from jumbo.json
+        <Jumbotron key={item.id} direction={item.direction}>
+          {/* Left hand-side */}
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          {/* Right hand-side */}
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
+}
