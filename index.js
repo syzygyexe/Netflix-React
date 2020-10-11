ad62a506a1f5c68d263f738e2c27cd929cbe2f7f
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-11 20:06:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import { render } from "react-dom";
 import "normalize.css";
 import App from "./App";
 import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
-render(
+render (
   <>
-    <GlobalStyles />
-    <App />
+    {/* Providing our whole application with the FirebaseContext */}
+    <FirebaseContext.Provider value ={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
