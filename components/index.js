3df5dd535ecf4ee2a4ca8e282a0f2827c47321cb
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-12 13:59:22 +0900

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,8 +1,9 @@
 // Reference file for all components
+// Reference file for all components
 export { default as Jumbotron } from "./jumbotron";
 export { default as Footer } from "./footer";
 export { default as Accordion } from "./accordion";
 export { default as OptForm } from "./opt-form";
 export { default as Header } from "./header";
 export { default as Feature } from "./feature";
-export
+export { default as Form } from "./form";
